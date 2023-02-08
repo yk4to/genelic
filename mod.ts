c01aaf9e5f255dc4ebe91518305dafed066bd215
fus1ondev <luke_s1220@yahoo.co.jp>
2023-02-08 15:23:49 +0900

    refactor: 💡 Set entry point

--- mod.ts
+++ mod.ts
@@ -4,25 +4,31 @@ import generateCommand from "./commands/generate.ts";
 import infoCommand from "./commands/info.ts";
 import listCommand from "./commands/list.ts";
 
-const info = new Command()
-  .description("Show license info.")
-  .arguments("[id:string]")
-  .action(infoCommand);
+const cli = async () => {
+  const info = new Command()
+    .description("Show license info.")
+    .arguments("[id:string]")
+    .action(infoCommand);
 
-const list = new Command()
-  .description("List all licenses.")
-  .action(listCommand);
+  const list = new Command()
+    .description("List all licenses.")
+    .action(listCommand);
 
-await new Command()
-  .name("genelic")
-  .version("1.0.0")
-  .description("Generate a license file for your project.")
-  .arguments("[id:string]")
-  .option("-o, --output <output:string>", "Output file name.",{ default: "LICENSE" })
-  .option("-f, --force", "Overwrite existing license file.")
-  .option("-i, --info", "Log license info.")
-  .option("-p, --parents", "Create parent directories as needed.")
-  .action(generateCommand)
-  .command("info", info)
-  .command("list", list)
-  .parse();
+  await new Command()
+    .name("genelic")
+    .version("1.0.0")
+    .description("Generate a license file for your project.")
+    .arguments("[id:string]")
+    .option("-o, --output <output:string>", "Output file name.",{ default: "LICENSE" })
+    .option("-f, --force", "Overwrite existing license file.")
+    .option("-i, --info", "Log license info.")
+    .option("-p, --parents", "Create parent directories as needed.")
+    .action(generateCommand)
+    .command("info", info)
+    .command("list", list)
+    .parse();
+}
+
+if (import.meta.main) {
+  cli();
+}
