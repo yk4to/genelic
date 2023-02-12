85df31cdcf4dec0622b36f5f1065d0127f9a52fe
fus1ondev <luke_s1220@yahoo.co.jp>
2023-02-12 13:08:20 +0900

    fix: 🐛 Remove unnecesarry console.log

--- utils/getLicenseFromId.ts
+++ utils/getLicenseFromId.ts
@@ -5,41 +5,40 @@ import readCargo from "./readCargo.ts";
 import readPyProject from "./readPyProject.ts";
 
 const getLicenseFromId = async (id: string | undefined, cwd: string) => {
-  const pkgJson = await readPackage({ cwd }).catch(() => undefined);
-
-  if (pkgJson?.license) {
-    const useFoundLicense = await Confirm.prompt({
-      message: `License "${pkgJson.license}" is set in package.json. Use it?`,
-    });
-    if (useFoundLicense) {
-      id = pkgJson.license;
-    }
-  }
-
-  const pyProjectToml = await readPyProject(cwd);
-
-  if (pyProjectToml) {
-    const license = pyProjectToml.tool?.poetry?.license;
-    if (license) {
+  if (!id) {
+    const pkgJson = await readPackage({ cwd }).catch(() => undefined);
+    if (pkgJson?.license) {
       const useFoundLicense = await Confirm.prompt({
-        message: `License "${license}" is set in pyproject.toml. Use it?`,
+        message: `License "${pkgJson.license}" is set in package.json. Use it?`,
       });
       if (useFoundLicense) {
-        id = license;
+        id = pkgJson.license;
       }
     }
-  }
-
-  const cargoToml = await readCargo(cwd);
-  if (cargoToml) {
-    console.log(cargoToml);
-    const license = cargoToml.package?.license;
-    if (license) {
-      const useFoundLicense = await Confirm.prompt({
-        message: `License "${license}" is set in Cargo.toml. Use it?`,
-      });
-      if (useFoundLicense) {
-        id = license;
+  
+    const pyProjectToml = await readPyProject(cwd);
+    if (pyProjectToml) {
+      const license = pyProjectToml.tool?.poetry?.license;
+      if (license) {
+        const useFoundLicense = await Confirm.prompt({
+          message: `License "${license}" is set in pyproject.toml. Use it?`,
+        });
+        if (useFoundLicense) {
+          id = license;
+        }
+      }
+    }
+  
+    const cargoToml = await readCargo(cwd);
+    if (cargoToml) {
+      const license = cargoToml.package?.license;
+      if (license) {
+        const useFoundLicense = await Confirm.prompt({
+          message: `License "${license}" is set in Cargo.toml. Use it?`,
+        });
+        if (useFoundLicense) {
+          id = license;
+        }
       }
     }
   }
