3262a9265e86a70e7bc7ca57b913133b2bc7dd09
fus1ondev <luke_s1220@yahoo.co.jp>
2023-04-09 10:39:35 +0900

    refactor: 💡 Optimize license detection process
    ✅ Closes: #8

--- utils/getLicense.ts
+++ utils/getLicense.ts
@@ -5,64 +5,56 @@ import readCargo from "./readCargo.ts";
 import readPyProject from "./readPyProject.ts";
 
 const getLicense = async (id: string | undefined, title: string | undefined, cwd: string) => {
-  id = licenses.find((license) => license.title === title)?.["spdx-id"];
+  if (!id) {
+    id = licenses.find(license => license.title === title)?.["spdx-id"];
+  }
 
   if (!id) {
     const pkgJson = await readPackage({ cwd }).catch(() => undefined);
-    if (pkgJson?.license) {
-      const useFoundLicense = await Confirm.prompt({
-        message: `License "${pkgJson.license}" is set in package.json. Use it?`,
-      });
-      if (useFoundLicense) {
-        id = pkgJson.license;
-      }
-    }
-  
-    const pyProjectToml = await readPyProject(cwd);
-    if (pyProjectToml) {
-      const license = pyProjectToml.tool?.poetry?.license;
-      if (license) {
-        const useFoundLicense = await Confirm.prompt({
-          message: `License "${license}" is set in pyproject.toml. Use it?`,
-        });
-        if (useFoundLicense) {
-          id = license;
-        }
-      }
-    }
-  
-    const cargoToml = await readCargo(cwd);
-    if (cargoToml) {
-      const license = cargoToml.package?.license;
-      if (license) {
-        const useFoundLicense = await Confirm.prompt({
-          message: `License "${license}" is set in Cargo.toml. Use it?`,
-        });
-        if (useFoundLicense) {
-          id = license;
-        }
-      }
-    }
+    id = await askToUseFoundLicense("package.json", pkgJson?.license);
   }
 
-  id = id ?? await Select.prompt({
-    message: "Select a license:",
-    options: licenses.map((license) => ({
-      name: license.title,
-      value: license["spdx-id"],
-    })),
-  });
+  if (!id) {
+    const pyProjectToml = await readPyProject(cwd);
+    id = await askToUseFoundLicense("pyproject.toml", pyProjectToml?.tool?.poetry?.license)
+  }
+  
+  if (!id) {
+    const cargoToml = await readCargo(cwd);
+    id = await askToUseFoundLicense("Cargo.toml", cargoToml?.package?.license)
+  }
+
+  if (!id) {
+    id = await Select.prompt({
+      message: "Select a license:",
+      options: licenses.map((license) => ({
+        name: license.title,
+        value: license["spdx-id"],
+      })),
+    });
+  }
+
   if (!id) {
     logger.error("No license selected.");
     Deno.exit(1);
   }
 
-  const license = licenses.find((license) => license["spdx-id"].toLowerCase() === id?.toLowerCase());
+  const license = licenses.find(license => license["spdx-id"].toLowerCase() === id?.toLowerCase());
   if (!license) {
     logger.error(`License with id "${id}" not found.`);
     Deno.exit(1);
   }
+
+  console.log(`${license.title} (${license["spdx-id"]}) has been selected.`);
+
   return license;
+}
+
+const askToUseFoundLicense = async (fileName: string, licenseId: string | undefined) => {
+  const useFoundLicense = await Confirm.prompt({
+    message: `License "${licenseId}" is set in ${fileName}. Use it?`,
+  });
+  return useFoundLicense ? licenseId : undefined;
 }
 
 export default getLicense;
