4a29e11649825be16e4f09ea9e8f567ce2a18168
GitHub <noreply@github.com>
2023-05-09 11:15:04 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -9,9 +9,9 @@ export {
   UpgradeCommand,
 } from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
 
-export * as path from "https://deno.land/std@0.176.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.176.0/fs/mod.ts";
-export * as toml from "https://deno.land/std@0.176.0/encoding/toml.ts";
+export * as path from "https://deno.land/std@0.186.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.186.0/fs/mod.ts";
+export * as toml from "https://deno.land/std@0.186.0/encoding/toml.ts";
 
-export { readPackage } from "https://esm.sh/read-pkg@7.1.0";
+export { readPackage } from "https://esm.sh/read-pkg@8.0.0/";
 export { getGitUserInfo } from "https://esm.sh/git-user-info@2.0.3";
