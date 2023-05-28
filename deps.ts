596c9c7b302f2ff9fc74f17578c2e88b6fdcc8e6
fus1ondev <luke_s1220@yahoo.co.jp>
2023-05-28 17:35:58 +0900

    refactor: 💡 Import dependencies from npm instead of esm.sh

--- deps.ts
+++ deps.ts
@@ -13,5 +13,5 @@ export * as path from "https://deno.land/std@0.176.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.176.0/fs/mod.ts";
 export * as toml from "https://deno.land/std@0.176.0/encoding/toml.ts";
 
-export { readPackage } from "https://esm.sh/read-pkg@7.1.0";
-export { getGitUserInfo } from "https://esm.sh/git-user-info@2.0.3";
+export { readPackage } from "npm:read-pkg";
+export { getGitUserInfo } from "npm:git-user-info";
